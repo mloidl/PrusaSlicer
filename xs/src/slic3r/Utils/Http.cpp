7db7c56cdc7412745e1b8f57cdc4d7ebaf3f9ac3
Vojtech Kral <vojtech@kral.hk>
2018-03-27 18:35:43 +0200

    Merge Http updates from the octoprint branch

--- xs/src/slic3r/Utils/Http.cpp
+++ xs/src/slic3r/Utils/Http.cpp
@@ -3,7 +3,6 @@
 #include <cstdlib>
 #include <functional>
 #include <thread>
-#include <iostream>
 #include <tuple>
 #include <boost/format.hpp>
 
@@ -37,15 +36,21 @@ struct Http::priv
 	::curl_slist *headerlist;
 	std::string buffer;
 	size_t limit;
+	bool cancel;
 
 	std::thread io_thread;
 	Http::CompleteFn completefn;
 	Http::ErrorFn errorfn;
+	Http::ProgressFn progressfn;
 
 	priv(const std::string &url);
 	~priv();
 
+	static bool ca_file_supported(::CURL *curl);
 	static size_t writecb(void *data, size_t size, size_t nmemb, void *userp);
+	static int xfercb(void *userp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow);
+	static int xfercb_legacy(void *userp, double dltotal, double dlnow, double ultotal, double ulnow);
+	std::string curl_error(CURLcode curlcode);
 	std::string body_size_error();
 	void http_perform();
 };
@@ -54,7 +59,8 @@ Http::priv::priv(const std::string &url) :
 	curl(::curl_easy_init()),
 	form(nullptr),
 	form_end(nullptr),
-	headerlist(nullptr)
+	headerlist(nullptr),
+	cancel(false)
 {
 	if (curl == nullptr) {
 		throw std::runtime_error(std::string("Could not construct Curl object"));
@@ -69,6 +75,29 @@ Http::priv::~priv()
 	::curl_easy_cleanup(curl);
 	::curl_formfree(form);
 	::curl_slist_free_all(headerlist);
+}
+
+bool Http::priv::ca_file_supported(::CURL *curl)
+{
+#ifdef _WIN32
+	bool res = false;
+#else
+	bool res = true;
+#endif
+
+	if (curl == nullptr) { return res; }
+
+#if LIBCURL_VERSION_MAJOR >= 7 && LIBCURL_VERSION_MINOR >= 48
+	::curl_tlssessioninfo *tls;
+	if (::curl_easy_getinfo(curl, CURLINFO_TLS_SSL_PTR, &tls) == CURLE_OK) {
+		if (tls->backend == CURLSSLBACKEND_SCHANNEL || tls->backend == CURLSSLBACKEND_DARWINSSL) {
+			// With Windows and OS X native SSL support, cert files cannot be set
+			res = false;
+		}
+	}
+#endif
+
+	return res;
 }
 
 size_t Http::priv::writecb(void *data, size_t size, size_t nmemb, void *userp)
@@ -88,6 +117,32 @@ size_t Http::priv::writecb(void *data, size_t size, size_t nmemb, void *userp)
 	return realsize;
 }
 
+int Http::priv::xfercb(void *userp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow)
+{
+	auto self = static_cast<priv*>(userp);
+	bool cb_cancel = false;
+
+	if (self->progressfn) {
+		Progress progress(dltotal, dlnow, ultotal, ulnow);
+		self->progressfn(progress, cb_cancel);
+	}
+
+	return self->cancel || cb_cancel;
+}
+
+int Http::priv::xfercb_legacy(void *userp, double dltotal, double dlnow, double ultotal, double ulnow)
+{
+	return xfercb(userp, dltotal, dlnow, ultotal, ulnow);
+}
+
+std::string Http::priv::curl_error(CURLcode curlcode)
+{
+	return (boost::format("%1% (%2%)")
+		% ::curl_easy_strerror(curlcode)
+		% curlcode
+	).str();
+}
+
 std::string Http::priv::body_size_error()
 {
 	return (boost::format("HTTP body data size exceeded limit (%1% bytes)") % limit).str();
@@ -99,6 +154,16 @@ void Http::priv::http_perform()
 	::curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 	::curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writecb);
 	::curl_easy_setopt(curl, CURLOPT_WRITEDATA, static_cast<void*>(this));
+
+	::curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+#if LIBCURL_VERSION_MAJOR >= 7 && LIBCURL_VERSION_MINOR >= 32
+	::curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, xfercb);
+	::curl_easy_setopt(curl, CURLOPT_XFERINFODATA, static_cast<void*>(this));
+	(void)xfercb_legacy;   // prevent unused function warning
+#else
+	::curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, xfercb);
+	::curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, static_cast<void*>(this));
+#endif
 
 #ifndef NDEBUG
 	::curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
@@ -117,16 +182,16 @@ void Http::priv::http_perform()
 	::curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_status);
 
 	if (res != CURLE_OK) {
-		std::string error;
-		if (res == CURLE_WRITE_ERROR) {
-			error = std::move(body_size_error());
-		} else {
-			error = ::curl_easy_strerror(res);
-		};
-
-		if (errorfn) {
-			errorfn(std::move(buffer), std::move(error), http_status);
+		if (res == CURLE_ABORTED_BY_CALLBACK) {
+			Progress dummyprogress(0, 0, 0, 0);
+			bool cancel = true;
+			if (progressfn) { progressfn(dummyprogress, cancel); }
 		}
+		else if (res == CURLE_WRITE_ERROR) {
+			if (errorfn) { errorfn(std::move(buffer), std::move(body_size_error()), http_status); }
+		} else {
+			if (errorfn) { errorfn(std::move(buffer), std::move(curl_error(res)), http_status); }
+		};
 	} else {
 		if (completefn) {
 			completefn(std::move(buffer), http_status);
@@ -180,7 +245,7 @@ Http& Http::remove_header(std::string name)
 
 Http& Http::ca_file(const std::string &name)
 {
-	if (p) {
+	if (p && priv::ca_file_supported(p->curl)) {
 		::curl_easy_setopt(p->curl, CURLOPT_CAINFO, name.c_str());
 	}
 
@@ -226,6 +291,12 @@ Http& Http::on_error(ErrorFn fn)
 	return *this;
 }
 
+Http& Http::on_progress(ProgressFn fn)
+{
+	if (p) { p->progressfn = std::move(fn); }
+	return *this;
+}
+
 Http::Ptr Http::perform()
 {
 	auto self = std::make_shared<Http>(std::move(*this));
@@ -245,6 +316,11 @@ void Http::perform_sync()
 	if (p) { p->http_perform(); }
 }
 
+void Http::cancel()
+{
+	if (p) { p->cancel = true; }
+}
+
 Http Http::get(std::string url)
 {
 	return std::move(Http{std::move(url)});
@@ -255,6 +331,25 @@ Http Http::post(std::string url)
 	Http http{std::move(url)};
 	curl_easy_setopt(http.p->curl, CURLOPT_POST, 1L);
 	return http;
+}
+
+bool Http::ca_file_supported()
+{
+	::CURL *curl = ::curl_easy_init();
+	bool res = priv::ca_file_supported(curl);
+	if (curl != nullptr) { ::curl_easy_cleanup(curl); }
+	return res;
+}
+
+std::ostream& operator<<(std::ostream &os, const Http::Progress &progress)
+{
+	os << "Http::Progress("
+		<< "dltotal = " << progress.dltotal
+		<< ", dlnow = " << progress.dlnow
+		<< ", ultotal = " << progress.ultotal
+		<< ", ulnow = " << progress.ulnow
+		<< ")";
+	return os;
 }
 
 
